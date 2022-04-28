a2dc100c9235e16845a83ca6015f19c78ae4d877
Youssefouagaou <ouagaouyoussef00@gmail.com>
2022-04-28 09:27:31 +0000

    first commit

--- src/main/java/mounir/me/springmvc/SpringMvcApplication.java
+++ src/main/java/mounir/me/springmvc/SpringMvcApplication.java
@@ -1,5 +1,6 @@
 package mounir.me.springmvc;
 
+import mounir.me.springmvc.entities.Patient;
 import mounir.me.springmvc.repositories.patientRepository;
 import mounir.me.springmvc.security.service.SecurityService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Date;
 
 @SpringBootApplication
 
@@ -26,37 +29,34 @@ public class SpringMvcApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-//        for (int i = 0; i < 10; i ++)
-//        {
-//            pr.save(
-//                    new Patient(null, "mounir"  , new Date(), Math.random() > 0.5, (int) (Math.random() * 10)));
-//        }
+        for (int i = 0; i < 10; i ++)
+        {
+            pr.save(
+                    new Patient(null, "youssef"  , new Date(), Math.random() > 0.5, (int) (Math.random() * 10) + 20));
+        }
 
-    try {
-//        securityService.saveNewUser("ismail", "ismail", "ismail");
-//        securityService.saveNewUser("mohammed", "mohammed", "mohammed");
-//        securityService.saveNewUser("mohammed", "mounir", "mounir");
-//
-//
-//        securityService.saveNewRole("USER", "utilisateur simple");
-//        securityService.saveNewRole("ADMIN", "Administrateur de l'application");
-//
-//
-//        securityService.addRoleToUser("mohammed", "ADMIN");
-//        securityService.addRoleToUser("mounir", "ADMIN");
-//        securityService.addRoleToUser("ismail", "USER");
-//        securityService.addRoleToUser("user1", "USER");
-//
-//        securityService.saveNewUser("admin", "admin", "admin");
-//
-//        securityService.addRoleToUser("admin", "ADMIN");
-//        securityService.addRoleToUser("admin", "USER");
-//
+    /*try {
+        securityService.saveNewUser("youssef", "youssef", "youssef");
+
+
+        securityService.saveNewRole("USER", "utilisateur simple");
+        securityService.saveNewRole("ADMIN", "Administrateur de l'application");
+
+
+        securityService.addRoleToUser("youssef", "ADMIN");
+        securityService.addRoleToUser("youssef", "ADMIN");
+
+
+
+
+       securityService.addRoleToUser("youssef", "ADMIN");
+        securityService.addRoleToUser("youssef", "USER");
+
 
     }
     catch(Exception e){
         e.printStackTrace();
-    }
+    }*/
 
     }
     @Bean
